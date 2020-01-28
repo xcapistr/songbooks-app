8de4cd5947812cb1f4f333b499cb707f0de712aa
Marek Capistrak <marek.capistrak@gmail.com>
2020-01-28 15:26:35 +0100

    update songlists UI

--- components/ArtistDetail.js
+++ components/ArtistDetail.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, FlatList, Image } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
-import SongCard from '../components/SongCard'
 import { GetArtist } from '../services/Db'
-import * as Colors from '../constants/Colors'
+import Colors from '../constants/Colors'
 
 const ListHeader = image => (
-  <Image style={styles.image} source={{ uri: image }} resizeMode="cover" />
+  <View>
+    <Image style={styles.image} source={{ uri: image }} resizeMode="cover" />
+    <View style={{ backgroundColor: '#ddd', height: 1 }}></View>
+  </View>
+)
+
+const ItemSeparator = () => (
+  <View style={{ backgroundColor: '#ddd', height: 1, marginLeft: 54 }}></View>
+)
+
+const ListFooter = () => (
+  <View style={{ backgroundColor: '#ddd', height: 1 }}></View>
 )
 
 const ArtistDetail = props => {
@@ -27,11 +45,12 @@ const ArtistDetail = props => {
         contentContainerStyle={{ paddingBottom: 5, paddingTop: 5 }}
         data={data.songs}
         ListHeaderComponent={ListHeader(data.image)}
+        ItemSeparatorComponent={ItemSeparator}
+        ListFooterComponent={ListFooter}
         renderItem={itemData => (
-          <SongCard
-            mini
-            name={itemData.item.name}
-            action={() => {
+          <TouchableOpacity
+            style={styles.songTouchable}
+            onPress={() => {
               props.navigation.navigate(
                 props.navigation.getParam('root') + 'L3',
                 {
@@ -42,7 +61,17 @@ const ArtistDetail = props => {
                 itemData.item.name
               )
             }}
-          />
+          >
+            <Image
+              style={styles.songIcon}
+              source={require('../assets/img/chord-icon.png')}
+              resizeMode="cover"
+            />
+            <View style={styles.songInfo}>
+              <Text style={styles.songName}>{itemData.item.name}</Text>
+            </View>
+            <Ionicons name="ios-arrow-forward" size={25} color="#ccc" />
+          </TouchableOpacity>
         )}
         keyExtractor={item => item.id}
       ></FlatList>
@@ -58,13 +87,35 @@ const styles = StyleSheet.create({
     width: 130,
     height: 130,
     alignSelf: 'center',
-    margin: 10,
+    margin: 20,
     borderRadius: 10,
     elevation: 4,
     shadowColor: Colors.darker,
     shadowOffset: { with: 0, height: 2 },
     shadowOpacity: 0.26,
     shadowRadius: 4
+  },
+  songTouchable: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  songIcon: {
+    width: 34,
+    height: 34,
+    borderRadius: 17,
+    // borderWidth: 1,
+    // borderColor: Colors.primary
+  },
+  songInfo: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 10
+  },
+  songName: {
+    fontSize: 18,
+    color: '#333'
   }
 })
 
--- components/BookDetail.js
+++ components/BookDetail.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import {
-  StyleSheet,
-  View,
-  FlatList,
-  Image,
-} from 'react-native'
+import { StyleSheet, View, FlatList, Image, TouchableOpacity, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
-import SongCard from '../components/SongCard'
 import { GetBook } from '../services/Db'
 import * as Colors from '../constants/Colors'
 
 const ListHeader = image => (
-  <Image style={styles.image} source={{ uri: image }} resizeMode="cover" />
+  <View>
+    <Image style={styles.image} source={{ uri: image }} resizeMode="cover" />
+    <View style={{ backgroundColor: '#ddd', height: 1 }}></View>
+  </View>
+)
+
+const ItemSeparator = () => (
+  <View style={{ backgroundColor: '#ddd', height: 1, marginLeft: 20 }}></View>
+)
+
+const ListFooter = () => (
+  <View style={{ backgroundColor: '#ddd', height: 1 }}></View>
 )
 
 const BookDetail = props => {
@@ -28,30 +34,36 @@ const BookDetail = props => {
 
   return (
     <View style={styles.screen}>
-        <FlatList
-          contentContainerStyle={{ paddingBottom: 5, paddingTop: 5 }}
-          ListHeaderComponent={ListHeader(data.image)}
-          data={data.songs}
-          renderItem={itemData => (
-            <SongCard
-              name={itemData.item.name}
-              artist={itemData.item.artist.name}
-              image={itemData.item.artist.image}
-              action={() => {
-                props.navigation.navigate(
-                  props.navigation.getParam('root') + 'L3',
-                  {
-                    type: 'song',
-                    id: itemData.item.id,
-                    name: itemData.item.name
-                  },
-                  itemData.item.name
-                )
-              }}
-            />
-          )}
-          keyExtractor={item => item.id}
-        ></FlatList>
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 5, paddingTop: 5 }}
+        ListHeaderComponent={ListHeader(data.image)}
+        ItemSeparatorComponent={ItemSeparator}
+        ListFooterComponent={ListFooter}
+        data={data.songs}
+        renderItem={itemData => (
+          <TouchableOpacity
+            style={styles.songTouchable}
+            onPress={() => {
+              props.navigation.navigate(
+                props.navigation.getParam('root') + 'L3',
+                {
+                  type: 'song',
+                  id: itemData.item.id,
+                  name: itemData.item.name
+                },
+                itemData.item.name
+              )
+            }}
+          >
+            <View style={styles.songInfo}>
+              <Text style={styles.songName}>{itemData.item.name}</Text>
+              <Text style={styles.songArtist}>{itemData.item.artist.name}</Text>
+            </View>
+            <Ionicons name="ios-arrow-forward" size={25} color="#ccc" />
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+      ></FlatList>
     </View>
   )
 }
@@ -64,13 +76,33 @@ const styles = StyleSheet.create({
     width: 130,
     height: 130,
     alignSelf: 'center',
-    margin: 10,
+    margin: 20,
     borderRadius: 10,
     elevation: 4,
     shadowColor: Colors.darker,
     shadowOffset: { with: 0, height: 2 },
     shadowOpacity: 0.26,
     shadowRadius: 4
+  },
+  songTouchable: {
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  songInfo: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 10
+  },
+  songName: {
+    fontSize: 18,
+    color: '#333'
+  },
+  songArtist: {
+    fontSize: 12,
+    color: '#888'
   }
 })
 
